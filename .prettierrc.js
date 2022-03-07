0298681f62bb6b8d9501d196a816eec4bc684423
박재현 <eric7213@naver.com>
2022-03-07 14:05:26 +0900

    [FIX]package

--- .prettierrc.js
+++ .prettierrc.js
@@ -5,6 +5,4 @@ module.exports = {
   tabWidth: 2, // 들여쓰기 너비는 2칸
   trailingComma: 'all', // 자세한 설명은 구글링이 짱이긴하나 객체나 배열 키:값 뒤에 항상 콤마를 붙히도록 formatting
   printWidth: 80, // 코드 한줄이 maximum 80칸
-  
-
 }
