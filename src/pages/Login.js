6618b389d651f8b49de0eb1bca1dc4e596a27ea2
박재현 <eric7213@naver.com>
2022-03-10 12:17:04 +0900

    [ADD]kakaoLoginButton

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,13 @@
 import * as React from 'react'
+import { KAKAO_AUTH_URL } from '../utils/OAuth'
 
 function Login() {
-  return <div>Login...</div>
+  return (
+    <a href={KAKAO_AUTH_URL}>
+      <img src="img/kakao_login_medium_wide.png" alt="카카오계정 로그인" />
+
+    </a>
+  )
 }
 
 export default Login
