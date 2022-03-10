b95a7ffce1aee041079c006f311a7e432e57b709
박재현 <eric7213@naver.com>
2022-03-10 12:34:51 +0900

    [FIX]kakaoLoginPage

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,27 @@
 import * as React from 'react'
+import styled from 'styled-components'
 import { KAKAO_AUTH_URL } from '../utils/OAuth'
 
 function Login() {
   return (
-    <a href={KAKAO_AUTH_URL}>
-      <img src="img/kakao_login_medium_wide.png" alt="카카오계정 로그인" />
+    <Wrapper>
 
-    </a>
+      <KakaoLogin href={KAKAO_AUTH_URL}>
+        <img src="img/kakao_login_medium_wide.png" alt="카카오계정 로그인" />
+      </KakaoLogin>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  position: relative;
+  width: 100%;
+  height: 100%;
+`
+const KakaoLogin = styled.a`
+  margin-top: 160%;
+`
 
 export default Login
