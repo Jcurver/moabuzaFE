73ac32ce3209adf476b8006af8b63311caffca5a
Aiden <fsd76005588@gmail.com>
2022-03-15 20:02:04 +0900

    [UPDATE] alert창 수정

--- src/pages/GroupBuzaDetail.js
+++ src/pages/GroupBuzaDetail.js
@@ -58,6 +58,10 @@ function GroupBuzaDetail() {
                 icon: 'success',
                 title: '목표달성!',
                 text: '이미 프로 도전러! ',
+                imageUrl: 'https://unsplash.it/400/200',
+                imageWidth: 400,
+                imageHeight: 200,
+                imageAlt: 'Custom image',
               })
             }}
           >
