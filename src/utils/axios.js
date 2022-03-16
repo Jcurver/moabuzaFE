8ec0dec4ef2d19a1390cfb1d5d903e784494851c
박재현 <eric7213@naver.com>
2022-03-16 23:40:17 +0900

    [FIX] axios refectoring24

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,5 +1,20 @@
 import axios from 'axios'
-import { getCookie } from './cookie'
+import { getCookie, setCookie } from './cookie'
+import {
+  BAD_REQUEST,
+  OK,
+  UNAUTHORIZED,
+  INTERNAL_SERVER_ERROR,
+  NOT_FOUND,
+} from '../constants/statusCode'
+import {
+  ACCESS_TOKEN_EXPIRED,
+  ACCESS_TOKEN_SIGNATURE_EXCEPTION,
+  ACCESS_TOKEN_MALFORMED,
+  REFRESH_TOKEN_EXPIRED,
+  REFRESH_TOKEN_SIGNATURE_EXCEPTION,
+  REFRESH_TOKEN_MALFORMED,
+} from '../constants/statusMessage'
 
 const instance = axios.create({
   baseURL: 'https://panghoon.shop',
@@ -14,6 +29,9 @@ instance.interceptors.request.use(async (config) => {
   const R_AUTH_TOKEN = getCookie('R-AUTH-TOKEN')
   config.headers['A-AUTH-TOKEN'] = `Bearer ${A_AUTH_TOKEN}`
   config.headers['R-AUTH-TOKEN'] = `Bearer ${R_AUTH_TOKEN}`
+  config.headers['Access-Control-Allow-Origin'] = '*'
+  config.headers['Access-Control-Allow-Credentials'] = true
+  config.headers.withCredentials = true
   return config
 })
 
@@ -32,8 +50,132 @@ export const apis = {
   getKakaoLogin: (code) => instance.get(`/user/kakao/callback?code=${code}`),
 }
 export const api = {
-  getPostButton: ()=> instance.post('/member/info', {
-    nickname: 'pangpang',
-    hero: 'hero1',
-  }),
+  getPostButton: () =>
+    instance.post('/member/info', {
+      nickname: 'pangpang',
+      hero: 'hero1',
+    }),
 }
+// instance.interceptors.response.use(
+//   (response) => {
+//     return response
+//   },
+
+//   async (error) => {
+//     const { data: responseData, config: originalRequest } = error.response
+
+//     if (
+//       responseData.responseMessage === null &&
+//       responseData.statusCode === null
+//     ) {
+//       if (process.env.REACT_APP_NODE_ENV === 'development') {
+//         console.error(error.response)
+//       }
+
+//       return Promise.reject(error)
+//     }
+
+//     if (responseData.status === INTERNAL_SERVER_ERROR) {
+//       if (process.env.REACT_APP_NODE_ENV === 'development') {
+//         console.error(error)
+//       }
+
+//       return Promise.reject(error)
+//     }
+
+//     if (responseData.statusCode === UNAUTHORIZED) {
+//       if (responseData.responseMessage === ACCESS_TOKEN_SIGNATURE_EXCEPTION) {
+//         if (process.env.REACT_APP_NODE_ENV === 'development') {
+//           console.error(error)
+//         }
+
+//         return Promise.reject(error)
+//       }
+
+//       if (responseData.responseMessage === ACCESS_TOKEN_MALFORMED) {
+//         if (process.env.REACT_APP_NODE_ENV === 'development') {
+//           console.error(error)
+//         }
+
+//         return Promise.reject(error)
+//       }
+//     }
+
+//     if (
+//       responseData.statusCode === BAD_REQUEST &&
+//       responseData.responseMessage === ACCESS_TOKEN_EXPIRED
+//     ) {
+//       if (process.env.REACT_APP_NODE_ENV === 'development') {
+//         console.error(responseData)
+//       }
+
+//       try {
+//         const { data } = await axios({
+//           method: 'GET',
+//           url: `${process.env.REACT_APP_BASE_URL}/user/loginCheck`,
+//           headers: {
+//             'Content-Type': 'application/json;charset=UTF-8',
+//             'R-AUTH-TOKEN': getCookie('habit-R-Token'),
+//           },
+//         })
+
+//         if (data.statusCode === OK) {
+//           setCookie('habit-A-Token', data.accessToken)
+//           originalRequest.headers['A-AUTH-TOKEN'] = `${data.accessToken}`
+//           return axios(originalRequest)
+//         }
+//       } catch (error) {
+//         if (
+//           error?.response?.data?.statusCode === BAD_REQUEST &&
+//           error?.response?.data?.responseMessage === REFRESH_TOKEN_EXPIRED
+//         ) {
+//           if (process.env.REACT_APP_NODE_ENV === 'development') {
+//             console.error(error)
+//           }
+
+//           return Promise.reject(error)
+//         }
+//         if (error?.response?.data?.statusCode === UNAUTHORIZED) {
+//           if (
+//             error?.response?.data?.responseMessage ===
+//             REFRESH_TOKEN_SIGNATURE_EXCEPTION
+//           ) {
+
+//             return Promise.reject(error)
+//           }
+//           if (
+//             error?.response?.data?.responseMessage === REFRESH_TOKEN_MALFORMED
+//           ) {
+//             if (process.env.REACT_APP_NODE_ENV === 'development') {
+//               console.error(error)
+//             }
+
+//             return Promise.reject(error)
+//           }
+//         }
+//         if (process.env.REACT_APP_NODE_ENV === 'development') {
+//           console.error(error)
+
+//         }
+//         return Promise.reject(error)
+//       }
+//     }
+
+//     if (error.response.data.statusCode === NOT_FOUND) {
+//       if (process.env.REACT_APP_NODE_ENV === 'development') {
+//         console.error(error)
+//       }
+//       return Promise.reject(error)
+//     }
+
+//     if (error.response.data.statusCode === INTERNAL_SERVER_ERROR) {
+//       if (process.env.REACT_APP_NODE_ENV === 'development') {
+//         console.error(error)
+//       }
+
+//       return Promise.reject(error)
+//     }
+
+//     return Promise.reject(error)
+//   },
+// )
