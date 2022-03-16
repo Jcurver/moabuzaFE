61d675db70184894a3239c4c0a50f360820eb4cb
박재현 <eric7213@naver.com>
2022-03-17 03:09:56 +0900

    [FIX] axios refectoring4566swer

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -16,32 +16,29 @@ import {
   REFRESH_TOKEN_MALFORMED,
 } from '../constants/statusMessage'
 
+
+
 const A_AUTH_TOKEN = getCookie('A-AUTH-TOKEN')
 const R_AUTH_TOKEN = getCookie('R-AUTH-TOKEN')
-const access = 'A-AUTH-TOKEN'
 
 const instance = axios.create({
   baseURL: 'https://panghoon.shop',
   headers: {
     'content-type': 'application/json;charset=UTF-8',
     accept: 'application/json,',
-    common: {
-      'A-AUTH-TOKEN': `Bearer ${A_AUTH_TOKEN}`,
-      'R-AUTH-TOKEN': `Bearer ${R_AUTH_TOKEN}`,
-    },
   },
 })
-// instance.interceptors.request.use(async (config) => {
-//   console.log('A_AUTH_TOKEN : ', A_AUTH_TOKEN)
-//   if (A_AUTH_TOKEN) {
-//     config.defaults.headers.common['A-AUTH-TOKEN'] = `Bearer ${A_AUTH_TOKEN}`
-//     config.defaults.headers.common['R-AUTH-TOKEN'] = `Bearer ${R_AUTH_TOKEN}`
-//   }
-//   // config.headers['Access-Control-Allow-Origin'] = '*'
-//   // config.headers['Access-Control-Allow-Credentials'] = true
-//   // config.headers.withCredentials = true
-//   return config
-// })
+instance.interceptors.request.use(async (config) => {
+  console.log('A_AUTH_TOKEN : ', A_AUTH_TOKEN)
+  if (A_AUTH_TOKEN) {
+    config.defaults.headers.common['A-AUTH-TOKEN'] = `Bearer ${A_AUTH_TOKEN}`
+    config.defaults.headers.common['R-AUTH-TOKEN'] = `Bearer ${R_AUTH_TOKEN}`
+  }
+  // config.headers['Access-Control-Allow-Origin'] = '*'
+  // config.headers['Access-Control-Allow-Credentials'] = true
+  // config.headers.withCredentials = true
+  return config
+})
 
 export const request = ({ ...options }) => {
   console.log(instance)
