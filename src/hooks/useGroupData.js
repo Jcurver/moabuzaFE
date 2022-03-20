139b7aa68a19265232af559b186236b2426f9f86
Aiden <fsd76005588@gmail.com>
2022-03-20 10:00:43 +0900

    [ADD] sweetalert button css, groupdata, challengedata

--- src/hooks/useGroupData.js
+++ src/hooks/useGroupData.js
@@ -1,13 +1,8 @@
 import { useQuery, useMutation, useQueryClient } from 'react-query'
 import { request } from '../utils/axios'
 
-export const useGroupData = (toggle, navigate, onSuccess, onError) => {
-  return useQuery(
-    ['group-data', navigate, toggle],
-    () => {
-      return request({ url: '/money/group', method: 'get' })
-    },
-    onSuccess,
-    onError,
-  )
+export const useGroupData = (navigate) => {
+  return useQuery(['group-data', navigate], () => {
+    return request({ url: '/money/group', method: 'get' })
+  })
 }
