df94bfb1a94669872f92cd2805fb9e5f11817b62
박재현 <eric7213@naver.com>
2022-03-24 13:04:54 +0900

    [ADD] LOGO 3

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -115,7 +115,7 @@ instance.interceptors.response.use(
   async (error) => {
     const { data: responseData, config: originalRequest } = error.response
     console.log('ERR RESPONSE', responseData, originalRequest)
-    if (responseData.message === 'Move to Login Page') {
+    if (responseData.message === '컨트롤러 Move to Login Page') {
       console.log('dddd')
 
       setMoveToLoginPage()
