f824c0e690ab6053126a13d3af2b93e8231ae09e
박재현 <eric7213@naver.com>
2022-03-28 19:40:17 +0900

    [FIX] main percent heroStep

--- src/apis/friendsData.js
+++ src/apis/friendsData.js
@@ -3,6 +3,6 @@ import { request } from "../utils/axios";
 
 export const useFriendsData = (navigate) => {
   return useQuery(['friend', navigate], () => {
-    return request({ url: '/friends', method: 'get' })
+    return request({ url: '/friend', method: 'get' })
   })
 }
