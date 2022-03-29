0621cd76425d5f16915e6c7d8168ffe031c94a33
Aiden <fsd76005588@gmail.com>
2022-03-29 12:23:32 +0900

    [UPDATE] loading page

--- src/pages/Loading.js
+++ src/pages/Loading.js
@@ -1,7 +1,24 @@
 import * as React from 'react'
+import styled from 'styled-components'
+import { LoadingIcon } from '../assets/icons/loading'
 
 function Loading() {
-  return <div>Loading...</div>
+  return (
+    <Wrapper>
+      <LoadingIconBox src={LoadingIcon} />
+    </Wrapper>
+  )
 }
+const Wrapper = styled.div`
+  position: relative;
+  width: 100%;
+`
 
+const LoadingIconBox = styled.img`
+  position: absolute;
+  width: 60px;
+  height: 60px;
+  left: 150px;
+  top: 308px;
+`
 export default Loading
