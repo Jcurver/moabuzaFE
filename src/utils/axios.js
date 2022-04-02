d4017bd37ac4999e0b128a0a194141c51e57d8f5
박재현 <eric7213@naver.com>
2022-04-02 21:33:54 +0900

    [FIX] buza create viewwidth

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -46,14 +46,14 @@ const addRefreshSubscriber = (callback) => {
 }
 
 instance.interceptors.request.use((config) => {
-  // setCookie(
-  //   'A-AUTH-TOKEN',
-  //   'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI1OGU2NzdjNCIsImlhdCI6MTY0ODg2MDEwMywiZXhwIjoxNjQ4OTQ2NTAzfQ.I3ajujkPI0_6gFrbjo4pMJvVNOGC2Xw4vhLCxAkxNKs',
-  // )
-  // setCookie(
-  //   'R-AUTH-TOKEN',
-  //   'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI1OGU2NzdjNCIsImlhdCI6MTY0ODg2MDEwMywiZXhwIjoxNjQ4OTQ2NTAzfQ.I3ajujkPI0_6gFrbjo4pMJvVNOGC2Xw4vhLCxAkxNKs',
-  // )
+  setCookie(
+    'A-AUTH-TOKEN',
+    'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI1OGU2NzdjNCIsImlhdCI6MTY0ODg2MDEwMywiZXhwIjoxNjQ4OTQ2NTAzfQ.I3ajujkPI0_6gFrbjo4pMJvVNOGC2Xw4vhLCxAkxNKs',
+  )
+  setCookie(
+    'R-AUTH-TOKEN',
+    'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI1OGU2NzdjNCIsImlhdCI6MTY0ODg2MDEwMywiZXhwIjoxNjQ4OTQ2NTAzfQ.I3ajujkPI0_6gFrbjo4pMJvVNOGC2Xw4vhLCxAkxNKs',
+  )
 
   const A_AUTH_TOKEN = getCookie('A-AUTH-TOKEN')
   const R_AUTH_TOKEN = getCookie('R-AUTH-TOKEN')
