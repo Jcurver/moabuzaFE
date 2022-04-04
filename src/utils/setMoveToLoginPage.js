0de7e6f33ac72c2dfe8304640212b7cfe3d8275a
박재현 <eric7213@naver.com>
2022-04-04 20:01:46 +0900

    [FIX] firebase

--- src/utils/setMoveToLoginPage.js
+++ src/utils/setMoveToLoginPage.js
@@ -1,8 +1,8 @@
-import { deleteCookie } from './cookie';
+import { deleteCookie } from './cookie'
 
 export const setMoveToLoginPage = () => {
-  deleteCookie('A-AUTH-TOKEN');
-  deleteCookie('R-AUTH-TOKEN');
+  deleteCookie('A-AUTH-TOKEN')
+  deleteCookie('R-AUTH-TOKEN')
   sessionStorage.clear()
-  window.location.href = `${process.env.REACT_APP_BASE_URL}/kakaologin`;
-};
+  window.location.href = `${process.env.REACT_APP_BASE_URL}/kakaologin`
+}
