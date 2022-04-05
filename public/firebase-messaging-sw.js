e62039b5d6cfdf0e1fa4473a2ab22505f4ad8d50
박재현 <eric7213@naver.com>
2022-04-06 02:45:33 +0900

    [FIX] sw cash

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,11 +2,11 @@
 /* eslint-disable no-var */
 
 
-importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js')
+// importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js')
 
-importScripts(
-  'https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js',
-)
+// importScripts(
+//   'https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js',
+// )
 
 const config = {
   apiKey: 'AIzaSyCGo8nqq7bA-zv87IqQNOS1y9xUJ2t4m1I',
@@ -35,7 +35,7 @@ firebase.initializeApp(config)
 // firebase.initializeApp({ messagingSenderId: 702007017171 })
 
 // 버전 업데이트
-const VERSION = 'V8'
+const VERSION = 'V9'
 const CACHE_NAME = `paper-cache_${VERSION}`
 const TEXT_CACHE_NAME = `paper-text_${VERSION}`
 
@@ -225,10 +225,10 @@ self.addEventListener('notificationclick', (event) => {
 })
 
 // Scripts for firebase and firebase messaging
-importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js')
-importScripts(
-  'https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js',
-)
+// importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js')
+// importScripts(
+//   'https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js',
+// )
 
 // Initialize the Firebase app in the service worker by passing the generated config
 var firebaseConfig = {
