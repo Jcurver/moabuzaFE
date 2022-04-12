e7be406a807d93cef1c8d3ce36bc8f8aba472b72
박재현 <eric7213@naver.com>
2022-04-12 23:37:52 +0900

    [FIX] no used file delete and clear annotation

--- src/pages/Fcmprac.js
+++ src/pages/Fcmprac.js
@@ -3,29 +3,25 @@ import React, { Suspense, lazy, useState, useEffect } from 'react'
 import '../utils/fcm'
 import axios from 'axios'
 import { fcmToken } from '../utils/ec2'
-// import { instance } from "./axios";
+
 
 function Fcmprac() {
   const bob = 'bog'
   function memberinfo() {
     axios.post(`https://panghoon.shop/member/info`)
-    // .then((res) => console.log('memberInfoRes:::', res))
-    // .catch((err) => console.log('memberInfoErr:::', err))
+
   }
   function pushnoti() {
     axios.post(`https://panghoon.shop/push`, {
       nickname: 'pangpang',
     })
-    // .then((res) => console.log('pushNotiRes:::', res))
-    // .catch((err) => console.log('pushNotiErr:::', err))
+
   }
   function ho() {
     axios.post(`https://panghoon.shop/ho`, {
       nickname: 'bog',
       token: fcmToken,
     })
-    // .then((res) => console.log('hoRes:::', res))
-    // .catch((err) => console.log('hoErr:::', err))
   }
 
   return (
--- src/utils/fcm.js
+++ src/utils/fcm.js
@@ -4,11 +4,6 @@ import firebase from 'firebase/compat/app'
 import { useEffect } from 'react'
 import { setItem } from './sessionStorage'
 
-// importScripts('https://www.gstatic.com/firebasejs/9.6.10/firebase-app-compat.js')
-
-// importScripts(
-//   'https://www.gstatic.com/firebasejs/9.6.10/firebase-messaging-compat.js',
-// )
 
 const config = {
   apiKey: 'AIzaSyCGo8nqq7bA-zv87IqQNOS1y9xUJ2t4m1I',
@@ -34,18 +29,10 @@ export const fcmToken = getToken(messaging, {
     return currentToken
   })
   .catch((err) => {
-    // console.log('An error occurred while retrieving token.', err)
+    console.log('An error occurred while retrieving token.', err)
   })
 
-// Notification.requestPermission().then((permission) => {
-//   if (permission === 'granted') {
-//     console.log('Notification permission granted.')
-//   } else {
-//     console.log('Unable to get permission to notify.')
-//   }
-// })
+
 
 // 포그라운드 메시지 수신
-onMessage(messaging, (payload) => {
-  // ...
-})
+onMessage(messaging, (payload) => {})
